3ccb13397583aa52f18e34086355877714ce9628
Clint Rollins <rollins.clint@gmail.com>
2017-11-03 01:24:52 -0600

    Initial Data and Directory Structure

---
+++ js/script.js
@@ -0,0 +1,19 @@
+d3.json('data/clint_family.json',function(data){
+    console.log(data);
+});
+
+/*
+/*"id":
+"firstname":
+"lastname":
+"birthdate":
+"hobby":
+"gender":
+"city":
+"state":
+"pathindividualphoto":null,
+"pathfamilyphoto":null,
+"spouse":
+"children": []
+}/*
+ */
