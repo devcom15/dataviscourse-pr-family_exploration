f52efa765590c2cb1296868ca31f277af4421156
Clint Rollins <rollins.clint@gmail.com>
2017-11-09 23:49:00 -0700

    Update family view

--- js/familyview.js
+++ js/familyview.js
@@ -7,7 +7,19 @@ class FamilyView {
     }
 
     loadView(id) {
+        let rootPath = "/data/photos/";
         let root = this.idMap[id];
+        let middle = d3.select("#middle");
+        middle.append("img")
+            .attr("src",rootPath + id + ".jpg");
+
+        let left = d3.select("#left");
+       // left.append("img")
+       //     .attr("src",this.idMap[this.parentMap[id][0]].id)
+
+        console.log(this.parentMap);
+
         console.log(root);
+        console.log(this.idMap[this.parentMap[id][0]].id)
     }
 }
