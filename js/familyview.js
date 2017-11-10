e39f1405d9d7d332508ca21951123c696391f791
Clint Rollins <rollins.clint@gmail.com>
2017-11-10 01:13:41 -0700

    Still sloppy, but you can click on images

--- js/familyview.js
+++ js/familyview.js
@@ -2,24 +2,80 @@ class FamilyView {
     constructor(idMap, parentMap) {
         this.idMap = idMap;
         this.parentMap = parentMap;
-
+        this.rootPath = "/data/photos/";
+        this.altBoy = this.rootPath + "boy_alt.jpg";
+        this.altGirl = this.rootPath + "girl_alt.jpg";
         this.loadView("clintonr87");
     }
 
     loadView(id) {
-        let rootPath = "/data/photos/";
+
+        d3.selectAll("img").remove();
         let root = this.idMap[id];
         let middle = d3.select("#middle");
         middle.append("img")
-            .attr("src",rootPath + id + ".jpg");
+            .attr("src",this.rootPath + id + ".jpg");
+        console.log(id);
+        console.log(this.idMap[this.parentMap[id]]);
+        let p1 = this.idMap[this.parentMap[id][0]];
+        let p2 = this.idMap[this.parentMap[id][1]]
+        this.addParent(p1);
+        this.addParent(p2);
+    }
 
+    addParent(p) {
+        if(p) {
+            if(p.gender == "M")
+                this.addLeft(p);
+            else
+                this.addRight(p);
+        }
+    }
+
+    addLeft(id) {
+        let self = this;
         let left = d3.select("#left");
-       // left.append("img")
-       //     .attr("src",this.idMap[this.parentMap[id][0]].id)
+        left.append("img")
+            .attr("src",function() {
+                if(self.imageExists(id)) {
+                    return self.rootPath + id.id + ".jpg"
+                }
+                else {
+                    return self.altBoy;
+                }
 
-        console.log(this.parentMap);
+            })
+            .on('click', function() {
+                self.loadView(id.id);
+            })
+    }
+    addRight(id) {
+        let self = this;
+        let right = d3.select("#right");
+        right.append("img")
+            .attr("src",function() {
+                if(self.imageExists(id)) {
+                    return self.rootPath + id.id + ".jpg"
+                }
+                else {
+                    return self.altGirl;
+                }
 
-        console.log(root);
-        console.log(this.idMap[this.parentMap[id][0]].id)
+            })
+            .on('click', function() {
+                self.loadView(id.id);
+            })
+            .on('hover', function() {
+
+            })
+    }
+
+
+    imageExists(person)
+    {
+        var http = new XMLHttpRequest();
+        http.open('HEAD', "/data/photos/" + person.id + ".jpg", false);
+        http.send();
+        return http.status!=404;
     }
 }
