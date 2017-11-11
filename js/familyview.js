8f8704071dc04cfcf7d0a6f6fb90f2d3d943ea4e
Clint Rollins <rollins.clint@gmail.com>
2017-11-10 22:11:33 -0700

    Draft of family view

--- js/familyview.js
+++ js/familyview.js
@@ -5,50 +5,140 @@ class FamilyView {
         this.rootPath = "/dataviscourse-pr-family_exploration/data/photos/";
         this.altBoy = this.rootPath + "boy_alt.jpg";
         this.altGirl = this.rootPath + "girl_alt.jpg";
+        this.altFamily = this.rootPath + "family_alt.jpg";
+
         this.loadView("clintonr87");
     }
 
     loadView(id) {
-
+        let self = this;
         d3.selectAll("img").remove();
         let root = this.idMap[id];
+
         let middle = d3.select("#middle");
-        middle.append("img")
-            .attr("src",this.rootPath + id + ".jpg");
-        console.log(id);
-        console.log(this.idMap[this.parentMap[id]]);
-        let p1 = this.idMap[this.parentMap[id][0]];
-        let p2 = this.idMap[this.parentMap[id][1]]
-        this.addParent(p1);
-        this.addParent(p2);
+        let bottom = d3.select("#bottom");
+        let left = d3.select("#left");
+
+        if(root.spouse || root.children.length > 0) {
+            if(root.familypath) {
+                middle.append("img")
+                    .attr("src",root.familypath)
+            } else {
+                middle.append("img")
+                    .attr("src",self.rootPath + id + ".jpg");
+            }
+            if(root.children.length > 0) {
+                root.children.forEach(function(c) {
+                    bottom.append("img")
+                        .attr("src", function() {
+                            return self.rootPath + c + ".jpg";
+                        })
+                        .attr("alt", self.idMap[c].firstname)
+                        .on('click', function() {
+                            self.loadView(c);
+                        });
+                })
+            }
+            console.log("id is " + id);
+            if(this.parentMap[id]) {
+                let p = this.idMap[this.parentMap[id][0]];
+                if(root.gender == "M") {
+                    this.addLeftFamily(p);
+                } else {
+                    this.addrightFamily(p);
+                }
+            }
+            if(root.spouse && this.parentMap[root.spouse]) {
+                let p = this.idMap[this.parentMap[root.spouse][0]];
+                console.log("Checking gender forspouse of " + root.id);
+                if(self.idMap[root.spouse].gender == "M") {
+                    this.addLeftFamily(p);
+                } else {
+                    this.addrightFamily(p);
+                }
+            }
+        } else {
+            middle.append("img")
+                .attr("src",this.rootPath + id + ".jpg");
+            if(this.parentMap[id]) {
+                let p1 = this.idMap[this.parentMap[id][0]];
+                let p2 = this.idMap[this.parentMap[id][1]];
+                this.addParent(p1);
+                this.addParent(p2);
+            }
+        }
     }
 
     addParent(p) {
         if(p) {
             if(p.gender == "M")
-                this.addLeft(p);
+                this.addLeftParent(p);
             else
-                this.addRight(p);
+                this.addRightParent(p);
         }
     }
 
-    addLeft(id) {
+    addLeftFamily(parent) {
+        let self = this;
+        let left = d3.select("#left");
+        left.append("img")
+            .attr("src", function() {
+                if(parent.familypath) {
+                    return parent.familypath;
+                }
+                else {
+                    return self.altFamily;
+                }
+            })
+            .on('click', function() {
+                self.loadView(parent.id);
+            })
+    }
+
+    addrightFamily(parent) {
+        let self = this;
+        let left = d3.select("#right");
+        left.append("img")
+            .attr("src", function() {
+                if(parent.familypath) {
+                    return parent.familypath;
+                }
+                else {
+                    return self.altFamily;
+                }
+            })
+            .on('click', function() {
+                self.loadView(parent.id);
+            })
+    }
+
+    addLeftParent(id) {
         let self = this;
         let left = d3.select("#left");
         left.append("img")
             .attr("src",function() {
-                  return self.rootPath + id.id + ".jpg";
+                if(self.imageExists(id.id)) {
+                    return self.rootPath + id.id + ".jpg"
+                }
+                else {
+                    return self.altBoy;
+                }
             })
             .on('click', function() {
                 self.loadView(id.id);
             })
     }
-    addRight(id) {
+    addRightParent(id) {
         let self = this;
         let right = d3.select("#right");
         right.append("img")
             .attr("src",function() {
-                    return self.rootPath + id.id + ".jpg";
+                if(self.imageExists(id.id)) {
+                    return self.rootPath + id.id + ".jpg"
+                }
+                else {
+                    return self.altGirl;
+                }
             })
             .on('click', function() {
                 self.loadView(id.id);
@@ -59,10 +149,10 @@ class FamilyView {
     }
 
 
-    imageExists(person)
+    imageExists(id)
     {
         var http = new XMLHttpRequest();
-        http.open('HEAD', "/data/photos/" + person.id + ".jpg", false);
+        http.open('HEAD', "/data/photos/" + id + ".jpg", false);
         http.send();
         return http.status!=404;
     }
