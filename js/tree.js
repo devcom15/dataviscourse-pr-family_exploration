b66f0d79639ce5d604e8ac9e80c1308a38639957
Aaron B Dayley <aaron.dayley@utah.edu>
2017-11-26 21:25:42 -0700

    Tree Mockup/Version 0.1

--- js/tree.js
+++ js/tree.js
@@ -7,14 +7,34 @@ class tree{
         this.generations = [];
         this.dataToDisplay = [];
 
+        this.maxMales = 20;
+        this.maxFemales = 30;
+
+        this.minAvgMaleAge = 60;
+        this.maxAvgMaleAge = 70;
+
+        this.minAvgFemaleAge = 61;
+        this.maxAvgFemaleAge = 72;
+
+        this.yearScale = null;
+        this.numOfGenderScale = null;
+        this.avgLifeScale = null;
+
+
         this.buildInitialDataSets();
+
+        this.femaleNumPoints = this.makeGenderNumPaths('F');
+        this.maleNumPoints = this.makeGenderNumPaths('M');
+
+        this.femaleAvgPoints = this.makeGenderAgePaths('F');
+        this.maleAvgPoints = this.makeGenderAgePaths('M');
 
         this.drawAxis();
     }
 
     drawAxis()
     {
-        var yearScale = d3.scaleLinear()
+        this.yearScale = d3.scaleLinear()
             .domain([1900, 2020])
             .range([0, 900]);
 
@@ -22,27 +42,122 @@ class tree{
             .domain([50, 950])
             .range([1900, 2020]);
 
-        let yearAxis = d3.axisTop(yearScale);
+        this.numOfGenderScale = d3.scaleLinear()
+            .range([45, 90])
+            .domain([(this.maxMales > this.maxFemales ? this.maxMales : this.maxFemales), 0]);
+
+        this.avgLifeScale = d3.scaleLinear()
+            .range([0, 45])
+            .domain([(this.maxAvgMaleAge > this.maxAvgFemaleAge ? this.maxAvgMaleAge : this.maxAvgFemaleAge),
+                (this.minAvgMaleAge > this.minAvgFemaleAge ? this.minAvgMaleAge : this.minAvgFemaleAge)]);
+
+        console.log('maxAVGMaleAge: ', this.maxAvgMaleAge);
+        console.log('minAVGMaleAge: ', this.minAvgMaleAge);
+        console.log('maxAVGFeMaleAge: ', this.maxAvgFemaleAge);
+        console.log('minAVGFeMaleAge: ', this.minAvgFemaleAge);
+
+        let genderAxis = d3.axisLeft(this.numOfGenderScale);
+
+        let avgLifeAxis = d3.axisLeft(this.avgLifeScale);
+
+        let yearAxis = d3.axisBottom(this.yearScale);
 
         let axisSVG = d3.select("#birth-year-selection").append("svg")
             .attr("width", 1000)
             .attr("height", 150);
 
+        //YEAR AXIS
         axisSVG.append("g").attr("width", 900)
             .attr("height", 150)
-            .call(yearAxis).attr("transform", "translate(50,100)")
+            .call(yearAxis).attr("transform", "translate(50,103)")
             .selectAll("text")
             .attr("font-size", "10");
 
-        axisSVG.append('text').text('Birth Year Selector: ')
-            .attr("x", 500)
-            .attr('y', 50)
-            .attr("dy", ".35em")
-            .style("text-anchor", "middle");
+        //AGE AXIS
+        axisSVG.append('g')
+            .attr('width', 50)
+            .attr('height', 45)
+            .call(avgLifeAxis.tickValues([14,30,46.6])).attr("transform", "translate(50, 3)")
+            .selectAll("text")
+            .attr("font-size", "10");
+
+        axisSVG.append('line')
+            .attr('x1', 50)
+            .attr('y1', 48.7)
+            .attr('x2', 950)
+            .attr('y2', 48.7)
+            .attr('stroke', 'black');
+
+
+        //GENDER NUMBER AXIS
+        axisSVG.append('g')
+            .attr('width', 50)
+            .attr('height', 45)
+            .call(genderAxis.tickValues([0,11, 22])).attr("transform", "translate(50,13)")
+            .selectAll("text")
+            .attr("font-size", "10");
+
+        //GENDER LINE GRAPHS
+        console.log(this.maleNumPoints);
+
+        var xScale = this.yearScale;
+        var yScale = this.numOfGenderScale;
+
+        var genderline = d3.line()
+            .x(function(d) { return xScale(d.x);})
+            .y(function(d) { return yScale(d.y);});
+
+        axisSVG.append('g').selectAll("path")
+            .data([this.maleNumPoints])
+            .enter().append("path")
+            .attr("class", "line")
+            .attr("d", genderline)
+            .attr('stroke', '#7FB4C6')
+            .attr('transform', 'translate(50,13)');
+
+        axisSVG.append('g').selectAll("path")
+            .data([this.femaleNumPoints])
+            .enter().append("path")
+            .attr("class", "line")
+            .attr("d", genderline)
+            .attr('stroke', '#C6ABBD')
+            .attr('transform', 'translate(50,13)');
+
+        //AGE LINE GRAPHS
+        yScale = this.avgLifeScale;
+
+        axisSVG.append('g').selectAll("path")
+            .data([this.maleAvgPoints])
+            .enter().append("path")
+            .attr("class", "line")
+            .attr("d", genderline)
+            .attr('stroke', '#7FB4C6')
+            .attr('transform', 'translate(50,3)');
+
+        axisSVG.append('g').selectAll("path")
+            .data([this.femaleAvgPoints])
+            .enter().append("path")
+            .attr("class", "line")
+            .attr("d", genderline)
+            .attr('stroke', '#C6ABBD')
+            .attr('transform', 'translate(50,3)');
+
+        console.log('dataLength: ', this.idMap);
+
+        //AGE LINE GRAPHS
+
+
+        // axisSVG.append('text').text('Birth Year Selector: ')
+        //     .attr("x", 500)
+        //     .attr('y', 50)
+        //     .attr("dy", ".35em")
+        //     .style("text-anchor", "middle");
 
         var filter = this.filterDrawData;
 
         var reDraw = this.drawTree;
+
+        var thisObject = this;
 
         this.dataToDisplay = this.generations.slice();
 
@@ -61,40 +176,198 @@ class tree{
 
             //console.log('MinYear: ', minYearRange, ' MaxYear: ', maxYearRange)
 
+            console.log('RedrawData', reDrawData);
 
-
-            filter(minYearRange, maxYearRange, reDrawData);
+            let filteredData = filter(minYearRange, maxYearRange, reDrawData);
 
             //console.log('JustBefore Draw', reDrawData);
 
-            reDraw(reDrawData);
+            reDraw(filteredData, thisObject);
         }
 
-        var brush = d3.brushX().extent([[50,75],[950,115]]).on("end", brushed);
+        var brush = d3.brushX().extent([[50,3],[950,103]]).on("end", brushed);
 
         axisSVG.append("g").attr("class", "brush").call(brush);
     }
 
-    filterDrawData(minYear, maxYear, data){
-        for(let i = 0; i < data.length; i++)
+    makeGenderNumPaths(gender)
+    {
+        let linePoints = [];
+
+        //used for min/max for axis
+        let calculatedValues = []
+
+
+        for(let i = 1900; i < 2021; i++)
         {
-            for(let j = 0; j < data[i].length; j++)
+            let countForYear = 0;
+
+            for(let j = 0; j < this.idMap.length; j++)
             {
-                if(!(data[i][j].birthdate >= minYear && data[i][j].birthdate <= maxYear))
+                let deathValue = 0;
+
+                if(this.idMap[j].death == null)
                 {
-                    data[i].splice(j, 1);
+                    deathValue = 2021;
+                }
+                else
+                {
+                    deathValue = this.idMap[j].death;
+                }
+
+                //console.log(deathValue);
+
+
+                if(+this.idMap[j].birthdate <= i && this.idMap[j].gender == gender && deathValue > i)
+                {
+                    countForYear += 1;
+                }
+            }
+
+            linePoints.push({'x': i, 'y': countForYear});
+
+            // if(i == 1900)
+            // {
+            //     path += 'M ' + this.yearScale(i) + ' ' + this.numOfGenderScale(countForYear) + ' ';
+            // }
+            // else if(i == 2020)
+            // {
+            //     path += 'L ' + this.yearScale(i) + ' ' + countForYear;
+            // }
+            // else
+            // {
+            //     path += 'L ' + this.yearScale(i) + ' ' + countForYear + ' ';
+            // }
+
+            calculatedValues.push(countForYear);
+        }
+        //console.log('LinePoints: ', linePoints);
+
+        if(gender == 'M')
+        {
+            this.maxMales = calculatedValues.reduce(function(a, b) {
+                return Math.max(a, b);});
+        }
+        if(gender == 'F')
+        {
+            this.maxFemales = calculatedValues.reduce(function(a, b) {
+                return Math.max(a, b);});
+        }
+
+        return linePoints;
+    }
+
+    makeGenderAgePaths(gender)
+    {
+        let linePoints = [];
+
+        //used for min/max for axis
+        let calculatedValues = []
+
+
+        for(let i = 1900; i < 2021; i++)
+        {
+            let countForYear = 0;
+            let agesSummed = 0;
+
+            for(let j = 0; j < this.idMap.length; j++)
+            {
+                let deathValue = 0;
+
+                if(this.idMap[j].death == null)
+                {
+                    deathValue = 2021;
+                }
+                else
+                {
+                    deathValue = this.idMap[j].death;
+                }
+
+                //console.log(deathValue);
+
+
+                if(+this.idMap[j].birthdate <= i && this.idMap[j].gender == gender && deathValue > i)
+                {
+                    countForYear += 1;
+                    agesSummed += (i - this.idMap[j].birthdate);
+                }
+            }
+
+            linePoints.push({'x': i, 'y': (agesSummed / countForYear)});
+
+            // if(i == 1900)
+            // {
+            //     path += 'M ' + this.yearScale(i) + ' ' + this.numOfGenderScale(countForYear) + ' ';
+            // }
+            // else if(i == 2020)
+            // {
+            //     path += 'L ' + this.yearScale(i) + ' ' + countForYear;
+            // }
+            // else
+            // {
+            //     path += 'L ' + this.yearScale(i) + ' ' + countForYear + ' ';
+            // }
+
+            calculatedValues.push(agesSummed / countForYear);
+        }
+        //console.log('LinePoints: ', linePoints);
+
+        if(gender == 'M')
+        {
+            this.maxAvgMaleAge = calculatedValues.reduce(function(a, b) {
+                return Math.max(a, b);});
+            this.minAvgMaleAge = calculatedValues.reduce(function(a, b) {
+                return Math.min(a, b);});
+        }
+        if(gender == 'F')
+        {
+            this.maxAvgFemaleAge = calculatedValues.reduce(function(a, b) {
+                return Math.max(a, b);});
+            this.minAvgFemaleAge = calculatedValues.reduce(function(a, b) {
+                return Math.min(a, b);});
+        }
+
+        return linePoints;
+    }
+
+    filterDrawData(minYear, maxYear, data){
+
+        /*var returnArray = data.slice();
+
+        console.log('Data: ', data);
+
+        console.log('Return Array', returnArray);*/
+
+        return data.map(d => {
+            return d.filter(innerD => {
+                return innerD.birthdate >= minYear && innerD.birthdate <= maxYear;
+            });
+        });
+
+        /*for(let i = 0; i < returnArray.length; i++)
+        {
+            for(let j = 0; j < returnArray[i].length; j++)
+            {
+                if(!(returnArray[i][j].birthdate >= minYear && returnArray[i][j].birthdate <= maxYear))
+                {
+                    returnArray[i].splice(j, 1);
                 }
             }
         }
 
         //console.log('DataAfterEdit:, ',data);
 
+        return returnArray;*/
+
     }
 
-    drawTree(drawData)
+    drawTree(drawData, treeObject)
     {
         //draw framing rectangles for generatinos
         d3.select('#tree').select('svg').remove();
+
+        var circleEvent = treeObject.circleOnClick;
+        var thisObject = this;
 
         var treeSVG = d3.select("#tree").append('svg')
             .attr('width', 1000)
@@ -119,11 +392,15 @@ class tree{
 
             genBlockCircles.exit().remove();
 
+
+
             genBlockCircles.enter().append('circle').merge(genBlockCircles)
                 .attr('cx', (d,i) => (i * 50))
                 .attr('cy', (i * 140) + 50)
                 .attr('r', 12)
+                .attr('id', d => d.id)
                 .attr('transform', 'translate(120,0)')
+                .on('click', d => circleEvent(d.id, thisObject))
                 .style('fill', d => {
                     if(d.gender === 'M')
                     {
@@ -140,6 +417,61 @@ class tree{
         }
 
         //this.dataToDisplay = this.generations.slice();
+    }
+
+    circleOnClick(id, treeObject)
+    {
+        let member = treeObject.getMemberEntry(id);
+
+        d3.select('#tree').selectAll('line').remove();
+
+        treeObject.drawLinesToChildren(member);
+
+        treeObject.drawLinesToParents(member)
+
+        //treeObject.drawTree(treeObject.dataToDisplay);
+
+        console.log('CircleClicked: ', member);
+    }
+
+    drawLinesToChildren(member)
+    {
+        let parentCircle = d3.select('#' + member.id);
+
+        console.log(member.id);
+
+        let parentCircleX = parentCircle.attr('cx');
+
+        console.log('ParentCircleX: ', parentCircleX);
+
+        let parentCircleY = parentCircle.attr('cy');
+
+        console.log('ParentCircleY: ', parentCircleY);
+
+        //Draw line to each child
+        for(let i = 0; i < member.children.length; i++)
+        {
+            let childCircle = d3.select('#' + member.children[i]);
+
+            let childCircleX = childCircle.attr('cx');
+            let childCircleY = childCircle.attr('cy');
+
+            d3.select('#tree').select('svg').append('line')
+                .attr('x1', parentCircleX)
+                .attr('y1', parentCircleY)
+                .attr('x2', childCircleX)
+                .attr('y2', childCircleY)
+                .attr('stroke', 'black')
+                .attr('transform', 'translate(120,0)')
+                .classed('line', true);
+
+        }
+
+    }
+
+    drawLinesToParents(member)
+    {
+
     }
 
     getMemberEntry(memberId)
@@ -223,7 +555,7 @@ class tree{
 
         this.dataToDisplay = this.generations.slice();
 
-        this.drawTree(this.dataToDisplay);
+        this.drawTree(this.dataToDisplay, this);
         //
         //identify list of spouses who are not part of the core family line we are analyzing
         // for(let i = 0; i < this.generations.length; i++)
